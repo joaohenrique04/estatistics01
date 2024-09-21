f9d84f0d38ac6ec164425c55aeee7f0cdba3a345
João Henrique Alencar <112728276+joaohenrique04@users.noreply.github.com>
2024-09-20 21:33:03 -0300

    Delete estatistics01 directory

--- estatistics01/exercicio01/banco_dados.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-bd = pd.read_csv('iris.csv')
-
-bd_setosa = bd[bd['Species'] == 'Iris-setosa']
-bd_versicolor = bd[bd['Species'] == 'Iris-versicolor']
-bd_virginica = bd[bd['Species'] == 'Iris-virginica']
