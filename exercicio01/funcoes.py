2f8013f0c37b22df10e67ef89ca3735b6bc01501
João Henrique Alencar <112728276+joaohenrique04@users.noreply.github.com>
2024-09-22 00:25:17 -0300

    commit q3

--- exercicio01/funcoes.py
+++ exercicio01/funcoes.py
@@ -1,4 +1,6 @@
 import scipy
+import matplotlib.pyplot as plt
+import math
 
 
 def calcula_assimetria(dados, var):
@@ -6,26 +8,26 @@ def calcula_assimetria(dados, var):
 
     print("Para", var, "a assimetria retornou:", end=" ")
     
-    if ass > 1:
+    if ass > 0.5:
         print("Cauda à Direita", end = ' ')
-    elif ass < -1:
+    elif ass < -0.5:
         print("Cauda à Esquerda", end = ' ')
     else:
         print("Simétrica", end = ' ')
-    print(ass)
+    print(round(ass, 2))
 
 def calcula_curtose(dados, var):
     curt = scipy.stats.kurtosis(dados)
 
     print("Para", var, "a curtose retornou:", end=" ")
 
-    if curt > 1:
+    if curt > 0.5:
         print("Acima do Ponto de Normalidade", end = ' ')
-    elif curt < -1:
+    elif curt < -0.5:
         print("Abaixo do Ponto de Normalidade", end = ' ')
     else:
         print("Ponto de Normalidade", end = ' ')
-    print(curt)
+    print(round(curt,2))
 
 def ret_p_value(dados, var):
     
@@ -34,8 +36,37 @@ def ret_p_value(dados, var):
     calcula_assimetria(dados, var)
     if p_v > 0.05:
         calcula_curtose(dados, var)
+    else:
+        print("Hipótese nula desconsiderada.")
     
     return p_v
+
+def hist(dados, title):
+    v_min = min(dados)
+    v_max = max(dados)
+    num_dados = len(dados)
+
+    v_bins = math.ceil(math.log2(num_dados) + 1)
+
+    plt.hist(dados, bins=v_bins, range=(v_min, v_max), color='skyblue', edgecolor='black')
+    plt.title(title)
+    plt.xlabel("Valor")
+    plt.ylabel("Frequência")
+    plt.show()
+
+def hist_bins(dados, title, p_bins):
+    v_min = min(dados)
+    v_max = max(dados)
+    num_dados = len(dados)
+
+    #v_bins = math.ceil(math.log2(num_dados) + 1)
+    v_bins = p_bins
+
+    plt.hist(dados, bins=p_bins, range=(v_min, v_max), color='skyblue', edgecolor='black')
+    plt.title(title)
+    plt.xlabel("Valor")
+    plt.ylabel("Frequência")
+    plt.show()
 
 def linha():
     print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
--- exercicio01/main.py
+++ exercicio01/main.py
@@ -1,30 +1,74 @@
 from banco_dados import *
 from funcoes import *
-import scipy
 # Id,SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm,Species
 
-
 # COMPRIMENTOS DE PÉTALA:
-
+"""""
 c_pet_setosa = round(bd_setosa["PetalLengthCm"].mean(), 2) ## 1.464 cm (3)
 c_pet_versicolor = round(bd_versicolor["PetalLengthCm"].mean(),2) ## 4.26 cm (2)
 c_pet_virginica = round(bd_virginica["PetalLengthCm"].mean(),2) ## 5.55 cm (1)
-
+"""""
 # print(c_pet_setosa, c_pet_versicolor, c_pet_virginica)
 
 # DISTRIBUICAO
 
 ## Comprimento da Sépala
 ### Setosa
+"""""
 p_value_cs_setosa = ret_p_value(bd_setosa["SepalLengthCm"], 'Comprimento de Sépala da Setosa') ## 0.460
+hist(bd_setosa["SepalLengthCm"], "SepalLengthCm - Setosa")
 linha()
 ### Versicolor
 p_value_cs_versicolor = ret_p_value(bd_versicolor["SepalLengthCm"], 'Comprimento de Sépala da Versicolor') ## 0.465
+hist(bd_versicolor["SepalLengthCm"], "SepalLengthCm - Versicolor")
 linha()
 ### Virginica
 p_value_cs_virginica = ret_p_value(bd_virginica["SepalLengthCm"], 'Comprimento de Sépala da Virginica') ## 0.258
+hist(bd_virginica["SepalLengthCm"], "SepalLengthCm - Virginica")
 linha()
 
-# AS 3 ESPECIES REJEITAM A HIPÓTESE NULA
+## Largura da Sépala
+### Setosa
+p_value_ls_setosa = ret_p_value(bd_setosa["SepalWidthCm"], 'Largura de Sépala da Setosa') ## 0.460
+hist(bd_setosa["SepalWidthCm"], "SepalWidthCm - Setosa")
+linha()
+### Versicolor
+p_value_ls_versicolor = ret_p_value(bd_versicolor["SepalWidthCm"], 'Largura de Sépala da Versicolor') ## 0.465
+hist(bd_versicolor["SepalWidthCm"], "SepalWidthCm - Versicolor")
+linha()
+### Virginica
+p_value_ls_virginica = ret_p_value(bd_virginica["SepalWidthCm"], 'Largura de Sépala da Virginica') ## 0.258
+hist(bd_virginica["SepalWidthCm"], "SepalWidthCm - Virginica")
+linha()
 
-print(p_value_cs_setosa, p_value_cs_versicolor, p_value_cs_virginica)
+## Comprimento da Pétala
+### Setosa
+p_value_cp_setosa = ret_p_value(bd_setosa["PetalLengthCm"], 'Comprimento de Pétala da Setosa') ## 0.460
+hist(bd_setosa["PetalLengthCm"], "PetalLengthCm - Setosa")
+linha()
+### Versicolor
+p_value_cp_versicolor = ret_p_value(bd_versicolor["PetalLengthCm"], 'Comprimento de Pétala da Versicolor') ## 0.465
+hist(bd_versicolor["PetalLengthCm"], "PetalLengthCm - Versicolor")
+linha()
+### Virginica
+p_value_cp_virginica = ret_p_value(bd_virginica["PetalLengthCm"], 'Comprimento de Pétala da Virginica') ## 0.258
+hist(bd_virginica["PetalLengthCm"], "PetalLengthCm - Virginica")
+linha()
+
+## Largura da Pétala
+### Setosa
+p_value_cp_setosa = ret_p_value(bd_setosa["PetalWidthCm"], 'Largura de Pétala da Setosa') ## 0.460
+hist(bd_setosa["PetalWidthCm"], "PetalWidthCm - Setosa")
+linha()
+### Versicolor
+p_value_cp_versicolor = ret_p_value(bd_versicolor["PetalWidthCm"], 'Largura de Pétala da Versicolor') ## 0.465
+hist(bd_versicolor["PetalWidthCm"], "PetalWidthCm - Versicolor")
+linha()
+### Virginica
+p_value_cp_virginica = ret_p_value(bd_virginica["PetalWidthCm"], 'Largura de Pétala da Virginica') ## 0.258
+hist(bd_virginica["PetalWidthCm"], "PetalWidthCm - Virginica")
+linha()
+"""""
+## USADOS PARA GERAR NOVAMENTE OS HISTOGRAMAS QUE A FUNCAO AUTOMATICA NAO RESOLVERAM
+# hist_bins(bd_setosa["PetalWidthCm"], "PetalWidthCm - Setosa", 6)
+# hist_bins(bd_virginica["PetalWidthCm"], "PetalWidthCm - Virginica", 6)
